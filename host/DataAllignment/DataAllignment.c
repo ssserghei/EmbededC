05291a77fe1283c19cc6905806844cab42a19359
ssserghei <ssserghei@gmail.com>
2020-08-12 19:26:57 +0300

    alignment

--- host/DataAllignment/DataAllignment.c
+++ host/DataAllignment/DataAllignment.c
@@ -9,15 +9,33 @@
 #include<stdint.h>
 #include<stdio.h>
 
+struct DataSet
+{
+	char 	data1;
+	int 	data2;
+	char 	data3;
+	short 	data4;
+
+};
+
+
+
 int main (void)
 {
-	char data1=0x11;
-	int data2=0XFFFEEEE;
-	char data3=0x22;
-	short data4=0xABCD;
+
+	struct DataSet data;
+
+		data.data1=0x11;
+		data.data2=0XFFFFEEEE;
+		data.data3=0x22;
+		data.data4=0xABCD;
+
+		uint8_t *ptr;
+
+		ptr=&data; //
+
 
 	printf("Memory address				Content\n");
 	printf("====================================\n");
-
 
 }
