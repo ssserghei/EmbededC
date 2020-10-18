9db9a59bf775b9b57f40cb36cb965ac44597033c
Serghei <ssserghei@gmail.com>
2020-10-18 23:21:39 +0300

    am introdus 2 masive de la tastatura

--- host/ArraySwapping/ArraySwapping.c
+++ host/ArraySwapping/ArraySwapping.c
@@ -29,27 +29,59 @@
 
 #include<stdio.h>
 #include<stdint.h>
+void wait_for_user_input(void);
 
 int main(void){
-	uint8_t nItemsOfArray1;
+	int32_t nItem1, nItem2;
+	printf(" Array Swapping program\n Enter no of elements of Array-1 and Array-2\n"); fflush(stdout);
+	scanf("%d %d",&nItem1,&nItem2);
 
-	uint8_t Array1[nItemsOfArray1];
-//	uint8_t Array2[nItemsOfArray2];
+	if((nItem1<0)||(nItem2<0)){
+		printf("Number of elements cannot be negative \n"); fflush(stdout);
+		wait_for_user_input();
+		return 0;
+		}//end if
 
-	printf("Enter no of elements of Array1 and Array2: \n"); fflush(stdout);
-	scanf("%d", &nItemsOfArray1);
+	int32_t array1[nItem1];
+	int32_t array2[nItem2];
 
-	for(uint8_t i=0; i<nItemsOfArray1; i++){
-	uint8_t cureEl;
-	printf("Enter element 0 of Array-1\n"); fflush(stdout);
-	scanf("%d",&cureEl);
-	Array1[i]=cureEl;
+	for(uint32_t i=0; i<nItem1; i++){
+		printf("Enter %d element of array1:",i); fflush(stdout);
+		scanf("%d", &array1[i]);
 	}//end for
 
+	printf("\n"); fflush(stdout);
 
-	for(uint8_t i=0; i<nItemsOfArray1; i++){
-	printf("Contents of : %d\t", Array1[i]=i); fflush(stdout);
+	for(uint32_t i=0; i<nItem2; i++){
+			printf("Enter %d element of array2:",i); fflush(stdout);
+			scanf("%d", &array2[i]);
 	}//end for
 
-	return 0;
+	display_array(array1, nItem1);
+	display_array(array2, nItem2);
+
+	printf("\n");
+
+	wait_for_user_input();
+
+}//End main
+
+void display_array(uint32_t *pArray1, uint32_t nitem)
+{
+	for(uint32_t i=0; i<nitem; i++){
+		printf("%d ",pArray1[i]); fflush(stdout); //*(pArray1+i)
+	}
+	printf("\n"); fflush(stdout);
 }
+
+void wait_for_user_input(void)
+{
+	printf("Press enter key to exit this application"); fflush(stdout);
+
+	while(getchar() != '\n')
+	    {
+	    	//just read the input buffer and do nothing
+	    }
+	    getchar();
+}
+
