18068e4f8aa7a75988c59cde241908aa41de2780
Serghei <ssserghei@gmail.com>
2020-12-16 23:00:46 +0200

    Создал блюпил

---
+++ target/BluePill_AllON/Debug/Src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c \
+../Src/sysmem.c 
+
+OBJS += \
+./Src/main.o \
+./Src/sysmem.o 
+
+C_DEPS += \
+./Src/main.d \
+./Src/sysmem.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/main.o: ../Src/main.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -DDEBUG -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/main.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Src/sysmem.o: ../Src/sysmem.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -DDEBUG -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/sysmem.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
---
+++ target/BluePill_AllON/Src/main.c
@@ -0,0 +1,48 @@
+
+/*Proiectul este realizat pe cablajul BluePill (MCU stm32f103c8t6)
+ * In acest proiect voi seta in 1 toate pinurile MCU*/
+/*Documentation Home page
+ * 	https://www.st.com/en/microcontrollers-microprocessors/stm32f103c8.html
+ *Datasheet
+ *	https://www.st.com/resource/en/datasheet/stm32f103c8.pdf
+ * Reference manual
+ * 	https://www.st.com/resource/en/reference_manual/cd00171190-stm32f101xx-stm32f102xx-stm32f103xx-stm32f105xx-and-st
+ *
+ * */
+
+/*BluePill pinout
+ *
+ * PC13		PB9
+ * PC14		PB8
+ * PC15		PB7
+ * PA0		PB6
+ * PA1		PB5
+ * PA2		PB4
+ * PA3		PB3
+ * PA4		PA15
+ * PA5		PA12
+ * PA6		PA11
+ * PA7		PA10
+ * PB0		PA9
+ * PB1		PA8
+ * PB10		PB15
+ * PB11		PB14
+ * 			PB13
+ * 			PB12
+ **/
+
+#include <stdint.h>
+#include <stdio.h>
+extern void initialise_monitor_handles(void);
+
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
+
+int main(void)
+{
+    initialise_monitor_handles();
+	printf("Hello World\n");
+	/* Loop forever */
+	for(;;);
+}
